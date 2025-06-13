3a33246672c4bd88eda8c8666aff1c69b58ce712
Fadl <chaos@efqr.dev>
2025-06-13 16:22:27 +0100

    add --is_large_file for processing very large files

--- stix2arango/__main__.py
+++ stix2arango/__main__.py
@@ -9,6 +9,7 @@ def parse_bool(value: str):
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Import STIX JSON into ArangoDB")
     parser.add_argument("--file", required=True, help="Path to STIX JSON file")
+    parser.add_argument("--is_large_file", action="store_true", help="Use large file mode [Use this mode when the bundle is very large, this will enable you stix2arango to chunk before loading into memory]")
     parser.add_argument("--database", required=True, help="ArangoDB database name")
     parser.add_argument("--create_db", default=True, type=parse_bool, help="whether or not to skip the creation of database, requires admin permission")
     parser.add_argument("--collection", required=True, help="ArangoDB collection name")
@@ -22,5 +23,5 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    stix_obj = Stix2Arango(args.database, args.collection, file=args.file, create_db=args.create_db, stix2arango_note=args.stix2arango_note, ignore_embedded_relationships=args.ignore_embedded_relationships, ignore_embedded_relationships_sro=args.ignore_embedded_relationships_sro, ignore_embedded_relationships_smo=args.ignore_embedded_relationships_smo)
+    stix_obj = Stix2Arango(args.database, args.collection, file=args.file, create_db=args.create_db, stix2arango_note=args.stix2arango_note, ignore_embedded_relationships=args.ignore_embedded_relationships, ignore_embedded_relationships_sro=args.ignore_embedded_relationships_sro, ignore_embedded_relationships_smo=args.ignore_embedded_relationships_smo, is_large_file=args.is_large_file)
     stix_obj.run()
